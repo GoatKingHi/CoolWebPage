b2ea780677239f32bad3e665677a6755afc5d3f4
Goat King <joaki2003@gmail.com>
2025-03-05 13:00:22 +0100

    WebPage made :)

---
+++ audio.js
@@ -0,0 +1,7 @@
+function enableSound() {
+    console.log("Hey");
+    document.body.style.backgroundImage = "url(Frikadelle.gif)";
+
+    document.getElementById("myAudio").play();
+    document.getElementById("button").style.display = "none";
+}
