21c12997ad43ab2c1f838358e9d2b8e2e87680c7
Goat King <joaki2003@gmail.com>
2025-03-05 13:22:17 +0100

    audio pis

--- audio.js
+++ audio.js
@@ -1,4 +1,7 @@
-document.getElementById("myAudio").pause();
+let audio = document.getElementById("myAudio");
+audio.autoplay = false; // Ensure autoplay is disabled
+audio.load(); // Reload the audio to apply changes
+
 function enableSound() {
     console.log("Hey");
     document.body.style.backgroundImage = "url(Frikadelle.gif)";
